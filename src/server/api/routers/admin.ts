d3952b3da355f2f823e5bd03aad179aad73ab90b
axyklee <longyklee@gmail.com>
2025-05-02 13:31:58 +0800

    fix: enhance response parsing to handle plus-separated values in addition to commas and spaces

--- src/server/api/routers/admin.ts
+++ src/server/api/routers/admin.ts
@@ -732,6 +732,8 @@ export const adminRouter = createTRPCRouter({
             response.push(...resp.split(",").map((r) => r.trim().toLowerCase()));
           } if (resp.trim().includes(" ")) {
             response.push(...resp.trim().split(" ").map((r) => r.trim().toLowerCase()));
+          } if (resp.trim().includes("+")) {
+            response.push(...resp.trim().split("+").map((r) => r.trim().toLowerCase()));
           } else {
             response.push(resp.trim().toLowerCase());
           }
